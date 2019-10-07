32adc9b99d5a3180401532c287db33ae8b89b6b6
Nikolay Vasilchuk <anonym.tsk@gmail.com>
2019-10-07 17:25:22 +0300

    Handle base request exceptions

--- starline/base_api.py
+++ starline/base_api.py
@@ -1,6 +1,7 @@
 """Base StarLine API."""
 import aiohttp
 import logging
+from typing import Optional
 from .const import DEFAULT_CONNECT_TIMEOUT, DEFAULT_TOTAL_TIMEOUT, DEFAULT_ENCODING, GET, POST
 
 _LOGGER = logging.getLogger(__name__)
@@ -17,70 +18,64 @@ class BaseApi:
         self._encoding: str = DEFAULT_ENCODING
 
     def set_timeout(self, total_timeout: int, connect_timeout: int = None) -> None:
+        """Set connection timeouts."""
         self._total_timeout = total_timeout
         if connect_timeout is not None:
             self._connect_timeout = connect_timeout
 
     def set_encoding(self, encoding: str) -> None:
+        """Set response encoding."""
         self._encoding = encoding
 
-    async def _request(
-        self,
-        method: str,
-        url: str,
-        params: dict = None,
-        data: dict = None,
-        json: dict = None,
-        headers: dict = None,
-    ) -> aiohttp.ClientResponse:
+    async def _request(self, method: str, url: str, params: dict = None, data: dict = None, json: dict = None, headers: dict = None) -> Optional[aiohttp.ClientResponse]:
         """Make request."""
 
-        response = await self._session.request(
-            method,
-            url,
-            params=params,
-            data=data,
-            json=json,
-            headers=headers,
-            timeout=aiohttp.ClientTimeout(
-                total=self._total_timeout,
-                connect=self._connect_timeout
-            ),
-        )
-        response.raise_for_status()
-        response.encoding = self._encoding
+        try:
+            response = await self._session.request(
+                method,
+                url,
+                params=params,
+                data=data,
+                json=json,
+                headers=headers,
+                timeout=aiohttp.ClientTimeout(
+                    total=self._total_timeout,
+                    connect=self._connect_timeout
+                ),
+            )
+            response.raise_for_status()
+            response.encoding = self._encoding
 
-        _LOGGER.debug("StarlineApi {} request: {}".format(method, url))
-        _LOGGER.debug("  Payload: {}".format(params))
-        _LOGGER.debug("  Data: {}".format(data))
-        _LOGGER.debug("  JSON: {}".format(json))
-        _LOGGER.debug("  Headers: {}".format(headers))
-        _LOGGER.debug("  Response: {}".format(response))
+            _LOGGER.debug("StarlineApi {} request: {}".format(method, url))
+            _LOGGER.debug("  Payload: {}".format(params))
+            _LOGGER.debug("  Data: {}".format(data))
+            _LOGGER.debug("  JSON: {}".format(json))
+            _LOGGER.debug("  Headers: {}".format(headers))
+            _LOGGER.debug("  Response: {}".format(response))
 
-        # TODO: Handle Exceptions
-        return response
+            return response
+        except aiohttp.ClientError as error:
+            _LOGGER.error("Request failed: %s", error)
+            return None
 
-    async def get(self, url: str, params: dict = None, headers: dict = None) -> dict:
+    async def _get(self, url: str, params: dict = None, headers: dict = None) -> Optional[dict]:
         """Make GET request."""
 
         response = await self._request(GET, url, params=params, headers=headers)
+        if response is None:
+            return None
+
         data = await response.json(content_type=None)
         _LOGGER.debug("  Data: {}".format(data))
         return data
 
-    async def post(
-        self,
-        url: str,
-        params: dict = None,
-        data: dict = None,
-        json: dict = None,
-        headers: dict = None,
-    ) -> dict:
+    async def _post(self, url: str, params: dict = None, data: dict = None, json: dict = None, headers: dict = None) -> Optional[dict]:
         """Make POST request."""
 
-        response = await self._request(
-            POST, url, params=params, data=data, json=json, headers=headers
-        )
+        response = await self._request(POST, url, params=params, data=data, json=json, headers=headers)
+        if response is None:
+            return None
+
         data = await response.json(content_type=None)
         _LOGGER.debug("  Data: {}".format(data))
         return data
