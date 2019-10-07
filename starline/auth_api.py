a39ad53e46771de0e4185244bd215bc62c9396bb
Nikolay Vasilchuk <anonym.tsk@gmail.com>
2019-10-07 17:34:13 +0300

    Handle API request exceptions

--- starline/auth_api.py
+++ starline/auth_api.py
@@ -19,13 +19,16 @@ class StarlineAuth(BaseApi):
             "appId": app_id,
             "secret": hashlib.md5(app_secret.encode(self._encoding)).hexdigest(),
         }
-        response = await self.get(url, params=payload)
+        response = await self._get(url, params=payload)
+        if response is None:
+            raise Exception("Failed to get application code")
 
         if int(response["state"]) == 1:
             app_code = response["desc"]["code"]
             _LOGGER.debug("Application code: {}".format(app_code))
             return app_code
-        raise Exception(response)
+
+        raise Exception("Invalid response state: {}", response["state"])
 
     async def get_app_token(self, app_id: str, app_secret: str, app_code: str) -> str:
         """Get application token for authentication."""
@@ -35,23 +38,18 @@ class StarlineAuth(BaseApi):
             "appId": app_id,
             "secret": hashlib.md5((app_secret + app_code).encode(self._encoding)).hexdigest(),
         }
-        response = await self.get(url, params=payload)
+        response = await self._get(url, params=payload)
+        if response is None:
+            raise Exception("Failed to get application token")
 
         if int(response["state"]) == 1:
             app_token = response["desc"]["token"]
             _LOGGER.debug("Application token: {}".format(app_token))
             return app_token
-        raise Exception(response)
 
-    async def get_slid_user_token(
-        self,
-        app_token: str,
-        user_login: str,
-        user_password: str,
-        sms_code: str = None,
-        captcha_sid: str = None,
-        captcha_code: str = None,
-    ) -> (bool, dict):
+        raise Exception("Invalid response state: {}", response["state"])
+
+    async def get_slid_user_token(self, app_token: str, user_login: str, user_password: str, sms_code: str = None, captcha_sid: str = None, captcha_code: str = None) -> (bool, dict):
         """Authenticate user by login, password and application token."""
 
         url = "https://id.starline.ru/apiV3/user/login/"
@@ -65,34 +63,37 @@ class StarlineAuth(BaseApi):
         if (captcha_sid is not None) and (captcha_code is not None):
             data["captchaSid"] = captcha_sid
             data["captchaCode"] = captcha_code
-        response = await self.post(url, params=payload, data=data)
+        response = await self._post(url, params=payload, data=data)
+        if response is None:
+            raise Exception("Failed to get user token")
 
         state = int(response["state"])
-        if (
-            (state == 1)
-            or (state == 2)
-            or (state == 0 and "captchaSid" in response["desc"])
-            or (state == 0 and "phone" in response["desc"])
-        ):
+        if (state == 1) or (state == 2) or (state == 0 and "captchaSid" in response["desc"]) or (state == 0 and "phone" in response["desc"]):
             return state, response["desc"]
-        raise Exception(response)
+
+        raise Exception("Invalid response state: {}", state)
 
     async def get_user_id(self, slid_token: str) -> (str, float, str):
         """Authenticate user by StarLineID token."""
-        # TODO: check response code
-        # TODO: Продливать куку
+        # TODO: Как-то продливать куку
 
         url = "https://developer.starline.ru/json/v2/auth.slid"
         data = {"slid_token": slid_token}
         response = await self._request(POST, url, json=data)
+        if response is None:
+            raise Exception("Failed to get SLNet token")
+
         json = await response.json(content_type=None)
+        if int(json["code"]) != 200:
+            raise Exception(json["codestring"])
 
         # Read cookie from headers because of bug https://gitlab.com/starline/openapi/issues/3
         cookie_header = response.headers.get("Set-Cookie")
         slnet = re.search("slnet=([^;]+);", cookie_header)
         expires = re.search("expires=([^;]+);", cookie_header)
+
         if slnet is None:
-            raise Exception(response)
+            raise Exception("Failed to get SLNet token")
 
         slnet_token = slnet.group(1)
         expires_time = None
@@ -103,5 +104,5 @@ class StarlineAuth(BaseApi):
             except:
                 pass
 
-        _LOGGER.debug("SLnet token: {}".format(slnet_token))
+        _LOGGER.debug("SLNet token: {}".format(slnet_token))
         return slnet_token, expires_time, json["user_id"]
